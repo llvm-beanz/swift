5568e202222bd622804ed43c390c071be324f287
Chris Bieneman <beanz@apple.com>
2017-02-03 13:33:10 -0800

    [CMake] Cleanup CMark library dependency
    This adds a variable that either resolves to the path of the CMark static library or the target name depending on whether this is a standalone or unified build.

--- cmake/modules/SwiftSharedCMakeConfig.cmake
+++ cmake/modules/SwiftSharedCMakeConfig.cmake
@@ -196,6 +196,8 @@ macro(swift_common_standalone_build_config_cmark product)
   set(CMARK_BUILD_INCLUDE_DIR "${PATH_TO_CMARK_BUILD}/src")
   include_directories("${CMARK_MAIN_INCLUDE_DIR}"
                       "${CMARK_BUILD_INCLUDE_DIR}")
+
+  set(CMARK_LIB ${CMARK_LIBRARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}cmark${CMAKE_STATIC_LIBRARY_SUFFIX})
 endmacro()
 
 # Common cmake project config for standalone builds.
@@ -248,7 +250,11 @@ macro(swift_common_unified_build_config product)
 
     set(CMARK_BUILD_INCLUDE_DIR "${PATH_TO_CMARK_BUILD}/src")
     set(CMARK_MAIN_INCLUDE_DIR "${CMARK_MAIN_SRC_DIR}/src")
+
+    set(CMARK_LIB libcmark_static)
   endif()
+
+  
 
   include_directories(
       "${CLANG_BUILD_INCLUDE_DIR}"
